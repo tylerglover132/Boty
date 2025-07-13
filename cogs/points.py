89cecf454e20340c3916e5ea3af0855c7c873d65
bub <tylerglover132@gmail.com>
2025-07-13 00:10:25 -0400

    Added trivia

--- cogs/points.py
+++ cogs/points.py
@@ -1,15 +1,55 @@
+import asyncio
 import json
 import discord
 from discord.ext import commands, tasks
 from pathlib import Path
 import random as r
+import aiohttp
+import html
+
+TRIVIA_URL = "https://opentdb.com/api.php?amount=1&difficulty=hard&type=multiple"
+
+config = json.loads(Path('config/config.json').read_text())
+
+class TriviaGame:
+
+    def __init__(self) -> None:
+        self.question: dict = None
+        self.already_answered = []
+
+    async def retrieve_question(self) -> None:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(TRIVIA_URL) as response:
+                response = await response.json()
+                self.question = response['results'][0]
+                self.question['question'] = html.unescape(self.question['question'])
+                self.question['correct_answer'] = html.unescape(self.question['correct_answer'])
+                self.question['incorrect_answers'] = html.unescape(self.question['incorrect_answers'])
+
+    def end_game(self) -> None:
+        self.question = None
+
+    def get_question(self) -> str:
+        return self.question['question']
+
+    def return_options(self) -> list:
+        options = [self.question['correct_answer']]
+        for option in self.question['incorrect_answers']:
+            options.append(option)
+        return options
+
+    def get_correct(self) -> str:
+        return self.question['correct_answer']
 
 class PointsCog(commands.Cog):
     """Cog that manages user points"""
 
     def __init__(self, bot: discord.ext.commands.Bot)-> None:
         self.bot = bot
+        self.trivia_game = None
         self.add_points_for_all.start()
+        self.trivia.start()
+
 
     @commands.command(name="trackpoints")
     async def trackpoints(self, ctx: discord.ext.commands.Context) -> None:
@@ -80,12 +120,73 @@ class PointsCog(commands.Cog):
         except Exception as e:
             self.bot.logger.error(f"Error occurred while updating all user points: {e}")
 
+    @tasks.loop(minutes=60.0)
+    async def trivia(self) -> None:
+        if r.randint(1, 100) < 20:
+            self.bot.logger.info("Starting trivia")
+            self.trivia_game = TriviaGame()
+            await self.trivia_game.retrieve_question()
+            message = "🎉 **Trivia Round Started!** 🎉\n\n"
+            message += "💡 The first person to respond with the right answer will win points!\n\n"
+            message += f"❓ *{self.trivia_game.get_question()}*\n\n"
+            for option in self.trivia_game.return_options():
+                message += (option + '\n')
+
+            trivia_channels = config['trivia_channel_id']
+            for channel_id in trivia_channels:
+                channel = self.bot.get_channel(channel_id)
+                await channel.send(message)
+
+            await asyncio.sleep(30 * 60)
+
+            if self.trivia_game:
+                self.trivia_game = None
+                for channel_id in trivia_channels:
+                    channel = self.bot.get_channel(channel_id)
+                    await channel.send("Looks like no one got the answer to the trivia question.\nBetter luck next time!")
+
+        else:
+            self.bot.logger.info('Trivia loop. No trivia this time.')
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.bot:
+            return
+
+        if self.trivia_game:
+            user_id = message.author.id
+            if user_id in self.trivia_game.already_answered:
+                await message.reply("You already answered! Don't be greedy!")
+                return
+
+            if message.content == self.trivia_game.get_correct():
+                points_list = json.loads(Path("data/points.json").read_text())
+                points_list[str(user_id)] += 100
+                try:
+                    with open("data/points.json", "w", encoding="utf-8") as f:
+                        json.dump(points_list, f, ensure_ascii=True, indent=4)
+                    await message.reply("Correct! 100 points added!")
+                    self.trivia_game = None
+                except Exception as e:
+                    await message.reply('Oh no! There was an issue! Trivia time over :(')
+                    self.bot.logger.error('Trivia error occurred. Ending trivia')
+                    self.trivia_game = None
+
+            else:
+                self.trivia_game.already_answered.append(user_id)
+                await message.reply('Nope! Better luck next time!')
+
     @add_points_for_all.before_loop
     async def before_add_points_for_all(self) -> None:
         """
         Before adding points, we make sure that the bot is running
         """
         await self.bot.wait_until_ready()
+
+    @trivia.before_loop
+    async def before_trivia(self) -> None:
+        await self.bot.wait_until_ready()
+
 
 async def setup(bot: discord.ext.commands.Bot) -> None:
     await bot.add_cog(PointsCog(bot))
