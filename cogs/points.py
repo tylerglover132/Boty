f145aa412a9f203d7bb981483003309948dd8e69
bub <tylerglover132@gmail.com>
2025-07-29 19:01:03 -0400

    Converted trivia to a separate cog and allowed for disabling all buttons after trivia

--- cogs/points.py
+++ cogs/points.py
@@ -17,112 +17,17 @@ TRIVIA_URL = "https://opentdb.com/api.php?amount=1&difficulty=hard&type=multiple
 
 config = json.loads(Path('config/config.json').read_text())
 
-class TriviaGame(discord.ui.View):
-
-    def __init__(self) -> None:
-        super().__init__(timeout=60*29)
-        self.question: dict = None
-        self.already_answered = []
-        self.answer_list = list()
-        self.winner: int = None
-        self.done = False
-
-    @discord.ui.button(label="A", style=discord.ButtonStyle.primary)
-    async def a(self, interaction: discord.Interaction, button: discord.ui.Button):
-        correct = self.answer_list[0] == self.get_correct()
-        await self.process_response(interaction.user.id, correct, interaction, button)
-
-    @discord.ui.button(label="B", style=discord.ButtonStyle.primary)
-    async def b(self, interaction: discord.Interaction,  button: discord.ui.Button):
-        correct = self.answer_list[1] == self.get_correct()
-        await self.process_response(interaction.user.id, correct, interaction, button)
-
-    @discord.ui.button(label="C", style=discord.ButtonStyle.primary)
-    async def c(self, interaction: discord.Interaction, button: discord.ui.Button):
-        correct = self.answer_list[2] == self.get_correct()
-        await self.process_response(interaction.user.id, correct, interaction, button)
-
-    @discord.ui.button(label="D", style=discord.ButtonStyle.primary)
-    async def d(self, interaction: discord.Interaction, button: discord.ui.Button):
-        correct = self.answer_list[3] == self.get_correct()
-        await self.process_response(interaction.user.id, correct, interaction, button)
-
-    async def process_response(self, user_id: int, correct: bool, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.question:
-            if user_id in self.already_answered:
-                print(f'User {interaction.user.name} already answered.')
-                await interaction.response.send_message("You already answered!", ephemeral=True)
-                return
-            else:
-                if self.winner:
-                    await interaction.response.send_message("Someone already got this question correct.", ephemeral=True)
-                    return
-                else:
-                    button.disabled = True
-                    self.already_answered.append(user_id)
-                    if correct:
-                        button.style = discord.ButtonStyle.green
-                        await interaction.response.edit_message(view=self)
-                        self.winner = interaction.user.id
-                        print(f'User {interaction.user.name} got the question correct.')
-                        try:
-                            async def post_form():
-                                async with aiohttp.ClientSession() as session:
-                                    async with session.post(
-                                        URL + '/db_points',
-                                        json = {"id": user_id, "points": 100}
-                                    ) as resp:
-                                        print(resp.status)
-                                        text = await resp.text()
-                                        print(text)
-                            await post_form()
-                            await interaction.followup.send(f"{interaction.user.name} was the first to get the message right! They will be awarded 100 points!")
-                        except Exception as e:
-                            print(f"Error: {e}")
-                            await interaction.followup.send("Something went wrong. Shutting down trivia.")
-                    else:
-                        button.style = discord.ButtonStyle.red
-                        await interaction.response.edit_message(view=self)
-                        await interaction.followup.send("Sorry. Wrong answer. You'll get it next time.", ephemeral=True)
-
-
-
-    async def retrieve_question(self) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(TRIVIA_URL) as response:
-                response = await response.json()
-                self.question = response['results'][0]
-                self.question['question'] = html.unescape(self.question['question'])
-                self.question['correct_answer'] = html.unescape(self.question['correct_answer'])
-                self.question['incorrect_answers'] = html.unescape(self.question['incorrect_answers'])
-
-    def get_question(self) -> str:
-        return self.question['question']
-
-    def return_options(self) -> list:
-        options = [self.question['correct_answer']]
-        for option in self.question['incorrect_answers']:
-            options.append(option)
-        r.shuffle(options)
-        self.answer_list = options
-        return options
-
-    def get_correct(self) -> str:
-        return self.question['correct_answer']
-
 class PointsCog(commands.Cog):
     """Cog that manages user points"""
 
     def __init__(self, bot: discord.ext.commands.Bot)-> None:
         self.bot = bot
-        self.trivia_game = None
         self.gamble_cooldown = False
         self.database = DB()
         self.session = aiohttp.ClientSession()
 
         # Start loops
         self.add_points_roulette.start()
-        self.trivia.start()
         self.refresh_gamble_cooldown.start()
         self.db_update.start()
 
@@ -165,44 +70,6 @@ class PointsCog(commands.Cog):
         else:
             await ctx.reply(f"You have {curr_user.points}!")
 
-
-    @tasks.loop(minutes=60.0)
-    async def trivia(self) -> None:
-        if r.randint(1, 100) < config['trivia_chance']:
-            self.bot.logger.info("Starting trivia")
-            self.trivia_game = TriviaGame()
-            await self.trivia_game.retrieve_question()
-            options_text = ''
-            options = self.trivia_game.return_options()
-            options_text += ('A: ' + options[0] + '\n')
-            options_text += ('B: ' + options[1] + '\n')
-            options_text += ('C: ' + options[2] + '\n')
-            options_text += ('D: ' + options[3] + '\n')
-            embed = discord.Embed(
-                title="🎉 Trivia Time!",
-                description=f"💡 The first person to respond with the right answer will win points!\n\n"
-                            f"❓ *{self.trivia_game.get_question()}*\n\n"
-                            f"{options_text}",
-            )
-            embed.set_footer(text="Reply with the correct answer exactly as shown. You have 30 minutes!")
-
-            trivia_channels = config['trivia_channel_id']
-            for channel_id in trivia_channels:
-                channel = self.bot.get_channel(channel_id)
-                await channel.send(embed=embed, view=self.trivia_game)
-
-            await asyncio.sleep(30 * 60)
-
-            if not self.trivia_game.winner:
-                correct_answer = self.trivia_game.get_correct()
-                self.trivia_game = None
-                for channel_id in trivia_channels:
-                    channel = self.bot.get_channel(channel_id)
-                    await channel.send(f"Looks like no one got the answer to the trivia question.\n The correct answer was {correct_answer}.\nBetter luck next time!")
-
-        else:
-            self.bot.logger.info('Trivia loop. No trivia this time.')
-
     @tasks.loop(minutes=5.0)
     async def db_update(self) -> None:
         try:
@@ -236,7 +103,6 @@ class PointsCog(commands.Cog):
 
     @db_update.before_loop
     @add_points_roulette.before_loop
-    @trivia.before_loop
     @refresh_gamble_cooldown.before_loop
     async def before_loops(self) -> None:
         await self.bot.wait_until_ready()
---
+++ cogs/trivia.py
@@ -0,0 +1,167 @@
+import asyncio
+import discord
+from discord.ext import commands, tasks
+import random as r
+import aiohttp
+import html
+import json
+from pathlib import Path
+
+
+URL = 'https://botly-api-rcyr.shuttle.app'
+TRIVIA_URL = "https://opentdb.com/api.php?amount=1&difficulty=hard&type=multiple"
+
+config = json.loads(Path('config/config.json').read_text())
+
+class TriviaGame(discord.ui.View):
+    def __init__(self, bot: discord.ext.commands.Bot):
+        super().__init__(timeout=60*29)
+        self.bot = bot
+        self.question: dict = None
+        self.already_answered = []
+        self.answer_list = []
+        self.winner: int = None
+        self.message = None
+
+    @discord.ui.button(label="A", style=discord.ButtonStyle.primary)
+    async def a(self, interaction: discord.Interaction, button: discord.ui.Button):
+        correct = self.answer_list[0] == self.get_correct()
+        await self.process_response(interaction.user.id, correct, interaction, button)
+
+    @discord.ui.button(label="B", style=discord.ButtonStyle.primary)
+    async def b(self, interaction: discord.Interaction, button: discord.ui.Button):
+        correct = self.answer_list[1] == self.get_correct()
+        await self.process_response(interaction.user.id, correct, interaction, button)
+
+    @discord.ui.button(label="C", style=discord.ButtonStyle.primary)
+    async def c(self, interaction: discord.Interaction, button: discord.ui.Button):
+        correct = self.answer_list[2] == self.get_correct()
+        await self.process_response(interaction.user.id, correct, interaction, button)
+
+    @discord.ui.button(label="D", style=discord.ButtonStyle.primary)
+    async def d(self, interaction: discord.Interaction, button: discord.ui.Button):
+        correct = self.answer_list[3] == self.get_correct()
+        await self.process_response(interaction.user.id, correct, interaction, button)
+
+    async def process_response(self, user_id: int, correct: bool, interaction: discord.Interaction, button: discord.ui.Button):
+        if user_id in self.already_answered:
+            self.bot.logger.info(f'User {interaction.user.name} already answered.')
+            await interaction.response.send_message("You already answered!", ephemeral=True)
+            return
+        else:
+            if self.winner:
+                await interaction.response.send_message("Someone already got this question correct.", ephemeral=True)
+                return
+            else:
+                button.disabled = True
+                self.already_answered.append(user_id)
+                if correct:
+                    button.style = discord.ButtonStyle.green
+                    await interaction.response.edit_message(view=self)
+                    self.winner = interaction.user.id
+                    self.bot.logger.info(f'User {interaction.user.id} got the question correct.')
+                    try:
+                        async def post_form():
+                            async with aiohttp.ClientSession() as session:
+                                async with session.post(
+                                    URL + '/db_points',
+                                    json = {"id": user_id, "points": 100}
+                                ) as resp:
+                                    self.bot.logger.info(f'DB API response {resp.status}')
+                                    text = await resp.text()
+                                    self.bot.logger.info(text)
+                        await post_form()
+                        await interaction.followup.send(f"{interaction.user.name} was the first to get the message right! They will be awarded 100 points!")
+                        await self.disable_buttons()
+                    except Exception as e:
+                        self.bot.logger.error(f"Error: {e}")
+                        await interaction.followup.send("Something went wrong. Shutting down trivia.")
+                        await self.disable_buttons()
+
+                else:
+                    button.style = discord.ButtonStyle.red
+                    await interaction.response.edit_message(view=self)
+                    await interaction.followup.send("Sorry. Wrong answer. You'll get it next time.", ephemeral=True)
+
+
+    async def start(self) -> None:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(TRIVIA_URL) as response:
+                response = await response.json()
+                self.question = response['results'][0]
+                self.question['question'] = html.unescape(self.question['question'])
+                self.question['correct_answer'] = html.unescape(self.question['correct_answer'])
+                self.question['incorrect_answers'] = html.unescape(self.question['incorrect_answers'])
+        options = [self.question['correct_answer']]
+        for option in self.question['incorrect_answers']:
+            options.append(option)
+        r.shuffle(options)
+        self.answer_list = options
+        await self.create_send_message()
+
+    async def end(self) -> None:
+        if not self.winner:
+            correct_answer = self.get_correct()
+            trivia_channels = config['trivia_channel_id']
+            for channel_id in trivia_channels:
+                channel = self.bot.get_channel(channel_id)
+                await channel.send(f"Looks like no one got the answer to the trivia question. \n The correct answer was {correct_answer}.\nBetter luck next time!")
+        await self.disable_buttons()
+
+    def get_question(self) -> str:
+        return self.question['question']
+
+    def return_options(self) -> list:
+        return self.answer_list
+
+    def get_correct(self) -> str:
+        return self.question['correct_answer']
+
+    async def create_send_message(self) -> None:
+        options_text = ''
+        options_text += ('A: ' + self.answer_list[0] + '\n')
+        options_text += ('B: ' + self.answer_list[1] + '\n')
+        options_text += ('C: ' + self.answer_list[2] + '\n')
+        options_text += ('D: ' + self.answer_list[3] + '\n')
+        embed = discord.Embed(
+            title="🎉 Trivia Time!",
+            description=f"💡 The first person to respond with the right answer will win points!\n\n"
+                        f"❓ *{self.get_question()}*\n\n"
+                        f"{options_text}",
+        )
+        embed.set_footer(text="Select your answer using the buttons below. You have 30 minutes!")
+        trivia_channels = config['trivia_channel_id']
+        for channel_id in trivia_channels:
+            channel = self.bot.get_channel(channel_id)
+            self. message = await channel.send(embed=embed, view=self)
+
+    async def disable_buttons(self) -> None:
+        for item in self.children:
+            if hasattr(item, "disabled"):
+                item.disabled = True
+        await self.message.edit_message(view=self)
+
+class TriviaCog(commands.Cog):
+    def __init__(self, bot: discord.ext.commands.Bot) -> None:
+        self.bot = bot
+        self.trivia_game = None
+
+        # Start loops
+        self.trivia.start()
+
+    @tasks.loop(minutes=60.0)
+    async def trivia(self) -> None:
+        if r.randint(1, 100) < 80:
+            self.bot.logger.info("Starting trivia")
+            self.trivia_game = TriviaGame(self.bot)
+            await self.trivia_game.start()
+            await asyncio.sleep(30 * 60)
+            await self.trivia_game.end()
+            self.trivia_game = None
+
+    @trivia.before_loop
+    async def before_loops(self) -> None:
+        await self.bot.wait_until_ready()
+
+async def setup(bot: discord.ext.commands.Bot) -> None:
+    await bot.add_cog(TriviaCog(bot))
